c8a6396c028d205657fc46d979672ad74cdf5f09
dubcik <dubcik>
2016-03-26 00:17:43 +0200

    first

---
+++ www/js/mat.js
@@ -0,0 +1,13 @@
+var ap={
+	var message;
+message = 'Hello!';
+
+alert( message ); 
+};
+							/*	var fi= parseInt(document.getElementById("first").value);
+								var Kelvin=273;
+							(	function someFunc(){
+										
+											alert(document.getElementById("first").value+" "+document.getElementById("last").value);
+											}
+											document.getElementById("btn").onclick = someFunc;);*/
