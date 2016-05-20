146cf29817297af8716392256304f71678d4ee8e
Andrei Dubovoi <andrei_dubovoi@mail.ru>
2016-05-20 18:10:13 +0300

    version 0.0.95

---
+++ www/js/ret.js
@@ -0,0 +1,22 @@
+function Name_Product(){
+				
+				 }
+			 
+			 
+
+document.getElementById("button").addEventListener("click", function () {
+	
+	
+	document.getElementById('td12').innerHTML="Ограничение  [-50 100]";
+	document.getElementById('td11').innerHTML="Ограничение  [-50 100]";
+	document.getElementById('td13').innerHTML="Ограничение  [-60 100]";
+	document.getElementById('td14').innerHTML="Ограничение  [-60 100]";
+	
+	document.getElementById('td12').value="Ограничение  [-50 100]";
+	document.getElementById('td11').value="Ограничение  [-50 100]";
+	document.getElementById('td13').value="Ограничение  [-60 100]";
+	document.getElementById('td14').value="Ограничение  [-60 100]";
+	
+});
+
+
